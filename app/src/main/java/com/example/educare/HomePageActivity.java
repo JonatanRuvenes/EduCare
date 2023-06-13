90951707301fedcb9d114b4119165e21d2c0a63e
JonatanRuvenes <116504321+JonatanRuvenes@users.noreply.github.com>
2023-06-13 20:08:00 +0300

    add notes to HomePageActivity

--- app/src/main/java/com/example/educare/HomePageActivity.java
+++ app/src/main/java/com/example/educare/HomePageActivity.java
@@ -1,11 +1,18 @@
 package com.example.educare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Context;import android.content.SharedPreferences;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,19 +27,26 @@ import java.util.Calendar;
 
 public class HomePageActivity extends AddMenuActivity {
 
-    // User info
+    //General data variables ***********************************************************************
+    //Firestore variables
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    //SharedPreferences variables
     SharedPreferences UserData;
+
+    //Activity variables
+    int day;
+    Calendar calendar = Calendar.getInstance();
+    ArrayList<String> Classes = new ArrayList<>();
+
+    //User variables
     String org;
     String tORs;
     String UserName;
-    int day;
 
-    // Importing data from Firestore
-    Calendar calendar = Calendar.getInstance();
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    ArrayList<String> Classes = new ArrayList<>();
+    //General data variables ***********************************************************************
 
-    // Views
+    //Views
     TextView Name;
     RecyclerView timetable;
 
@@ -41,21 +55,28 @@ public class HomePageActivity extends AddMenuActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        //getting general vars *********************************************************************
         UserData = getSharedPreferences("UserData", Context.MODE_PRIVATE);
         org = UserData.getString("org", "not found");
         tORs = UserData.getString("tORs", "not found");
         UserName = UserData.getString("UserName", "not found");
         day = calendar.get(Calendar.DAY_OF_WEEK);
+        //getting general vars *********************************************************************
+
+        //Find views
+        Name = findViewById(R.id.TVName);
+        timetable = findViewById(R.id.RVTimetable);
+
+
+        //Sets views
 
         // General vars
-        Name = findViewById(R.id.TVName);
+
         Name.setText(UserName);
 
         // Building lessons from Firestore
-        timetable = findViewById(R.id.RVTimetable);
-        DocumentReference docRef = db.collection("organizations")
-                .document(org).collection(tORs).document(UserName);
-        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        db.collection("organizations").document(org).collection(tORs).document(UserName)
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
                 if (documentSnapshot.exists()) {
@@ -66,7 +87,6 @@ public class HomePageActivity extends AddMenuActivity {
         });
     }
 
-    //TODO: fix the runtime of this method
     public void updateTimeTable() {
         if(Classes == null) {return;}
         ArrayList<Lesson> lessons = new ArrayList<>();
@@ -113,6 +133,126 @@ public class HomePageActivity extends AddMenuActivity {
                         }
                     }
                 });
+            }
+        }
+
+    }
+
+    private class Lesson {
+        String subject;
+        Time Start;
+        Time End;
+        String ClassroomID;
+
+        public Lesson(String className, Time start, Time end, String classroomID) {
+            this.subject = className;
+            Start = start;
+            End = end;
+            ClassroomID = classroomID;
+        }
+
+        public String getClassroomID() {
+            return ClassroomID;
+        }
+
+        public void setClassroomID(String classroomID) {
+            ClassroomID = classroomID;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public void setSubject(String subject) {
+            this.subject = subject;
+        }
+
+        public Time getStart() {
+            return Start;
+        }
+
+        public void setStart(Time start) {
+            Start = start;
+        }
+
+        public Time getEnd() {
+            return End;
+        }
+
+        public void setEnd(Time end) {
+            End = end;
+        }
+    }
+    public class LessonAdapter extends RecyclerView.Adapter<LessonAdapter.LessonViewHolder>{
+        ArrayList<Lesson> lessons;
+        String tORs;
+
+
+        public LessonAdapter(ArrayList<Lesson> lessons, String tORs, FragmentActivity fragmentActivity) {
+            this.lessons = lessons;
+            this.tORs = tORs;
+        }
+
+        @NonNull
+        @Override
+        public LessonViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View lessonView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.recycleritem_lesson,parent,false);
+            return new LessonViewHolder(lessonView);
+        }
+
+        @Override
+        public int getItemCount() {
+            return lessons.size();
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull LessonViewHolder holder, int position) {
+            Lesson currentLesson = lessons.get(position);
+
+            holder.className.setText(currentLesson.getSubject());
+            holder.Time.setText(currentLesson.getStart().toString() +"-"+ currentLesson.getEnd().toString());
+
+            // Set OnClickListener on the view
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Start new activity with the ClassroomID as extra
+                    if (tORs.equals("Teacher")){
+                        Intent i = new Intent(view.getContext(), LessonTActivity.class);
+                        i.putExtra("ClassroomId", lessons.get(holder.getAdapterPosition()).getClassroomID());
+                        i.putExtra("Subject", lessons.get(holder.getAdapterPosition()).getSubject());
+                        i.putExtra("StartHour", lessons.get(holder.getAdapterPosition()).getStart().Hour);
+                        i.putExtra("StartMinutes", lessons.get(holder.getAdapterPosition()).getStart().Minutes);
+                        i.putExtra("EndHour", lessons.get(holder.getAdapterPosition()).getEnd().Hour);
+                        i.putExtra("EndMinutes", lessons.get(holder.getAdapterPosition()).getEnd().Minutes);
+                        view.getContext().startActivity(i);
+                    }
+                    else {
+                        Bundle bundle = new Bundle();
+                        bundle.putString("ClassroomId", lessons.get(holder.getAdapterPosition()).getClassroomID());
+                        AppCompatActivity activity = (AppCompatActivity) view.getContext();
+                        LessonSFragment fragment = new LessonSFragment();
+                        fragment.setArguments(bundle);
+                        activity.getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.FContainerLessonS,fragment)
+                                .addToBackStack(null).commit();
+                    }
+
+                }
+            });
+        }
+
+        public static class LessonViewHolder extends RecyclerView.ViewHolder{
+
+            public TextView className;
+            public TextView Time;
+
+            public LessonViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                className = itemView.findViewById(R.id.RITVClassName);
+                Time = itemView.findViewById(R.id.RITVTime);
             }
         }
     }
